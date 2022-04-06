2a4ed1a2c1a9a597f817b0b8511f3215aa0347b0
2tefan <2tefan@tuta.io>
2022-04-06 20:55:37 +0200

    Add motion path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,28 @@
     <div ref="input" class="square" @click="submit"></div>
     <CommeanLogo />
     <Subtitle />
+
+    <div class="motion-path">
+      <div ref="input" class="square el" @click="submit"></div>
+      <svg
+        width="210mm"
+        height="297mm"
+        version="1.1"
+        viewBox="0 0 210 297"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m29.961-0.18323v86.056c0 15.284 0.0041 15.321 15.321 15.321h167.34"
+          fill="none"
+          stroke="#000"
+          stroke-width=".26458px"
+        />
+      </svg>
+    </div>
   </main>
 </template>
 
-<style >
+<style>
 .square {
   background-color: red;
   height: 10px;
@@ -18,6 +36,7 @@
 <script lang="ts">
 import CommeanLogo from "./../components/icons/CommeanLogo.vue";
 import Subtitle from "./../components/icons/Subtitle.vue";
+import anime from "animejs/lib/anime.es.js";
 
 export default {
   mounted() {
@@ -26,6 +45,22 @@ export default {
   components: {
     CommeanLogo,
     Subtitle,
+  },
+  methods: {
+    submit() {
+      console.log("h");
+      var path = anime.path(".motion-path path");
+
+      anime({
+        targets: ".motion-path .el",
+        translateX: path("x"),
+        translateY: path("y"),
+        rotate: path("angle"),
+        easing: "linear",
+        duration: 2000,
+        loop: true,
+      });
+    },
   },
 };
 </script>
