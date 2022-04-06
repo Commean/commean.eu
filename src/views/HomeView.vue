c86d6a280251987b00efdd2347a93f53b3c08fe4
2tefan <2tefan@tuta.io>
2022-04-06 12:27:38 +0200

    Setup anime

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,35 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <main>
     <p>Hi</p>
+    <div ref="input" class="square el" @click="submit"></div>
   </main>
 </template>
+
+<style >
+.square {
+  background-color: red;
+  height: 10px;
+  width: 10px;
+}
+</style>
+
+<script lang="ts">
+import anime from "animejs/lib/anime.es.js";
+
+export default {
+  methods: {
+    submit() {
+      console.log(this.$refs.input);
+      anime({
+        targets: this.$refs.input,
+        points: "64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96",
+        baseFrequency: 0,
+        scale: 1,
+        loop: true,
+        direction: "alternate",
+        easing: "easeInOutExpo",
+      });
+    },
+  },
+};
+</script>
