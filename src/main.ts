f91a46ffd5ef98866e43255dd6629a36fc666ae7
2tefan <2tefan@tuta.io>
2022-04-07 22:24:57 +0200

    Add vehicles to map

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import VueLogger from 'vuejs3-logger';
+
+const isProduction = process.env.NODE_ENV === 'production';
+ 
+const options = {
+    isEnabled: true,
+    logLevel : isProduction ? 'error' : 'debug',
+    stringifyArguments : false,
+    showLogLevel : true,
+    showMethodName : true,
+    separator: '|',
+    showConsoleColors: true
+};
+
+const app = createApp(App).use(VueLogger, options);
 
 app.use(router)
 
