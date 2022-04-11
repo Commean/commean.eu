6a2dd99e324342ae55516600b6e12b6b0a7ac1d6
2tefan <2tefan@tuta.io>
2022-04-11 11:31:33 +0200

    Animatte box shadow

--- src/components/cards/FloatingCardBase.vue
+++ src/components/cards/FloatingCardBase.vue
@@ -15,7 +15,27 @@
   display: inline-grid;
   background-color: white;
   padding: 10px;
-  box-shadow: 0 5px 10px 0 black;
+  box-shadow: 0 5px 10px 0 inset black;
   border-radius: 25px;
 }
 </style>
+
+<script lang="ts">
+import anime from "animejs/lib/anime.es.js";
+
+export default {
+  mounted() {
+    anime({
+      targets: ".card",
+      keyframes: [
+        { boxShadow: "0 5px 10px 0 inset black" },
+        { boxShadow: "0 2px 0 0 inset black" },
+        { boxShadow: "0 0 0 0 black" },
+        { boxShadow: "0 5px 10px 0 black" },
+      ],
+      duration: 1000,
+      delay: 0,
+    });
+  },
+};
+</script>
