97786c31508a30155c112ddf7377919c89dac759
2tefan <2tefan@tuta.io>
2022-04-11 11:04:07 +0200

    Remove access to child

--- src/components/cards/FloatingCardWide.vue
+++ src/components/cards/FloatingCardWide.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- https://gist.github.com/Loilo/73c55ed04917ecf5d682ec70a2a1b8e2 -->
-  <FloatingCardBase :is="child">
+  <FloatingCardBase>
     <template v-for="(_, name) in $slots" v-slot:[name]="slotData"
       ><slot :name="name" v-bind="slotData"
     /></template>
