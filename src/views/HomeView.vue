88da4a95d7f453aafaced18974158b37cd73d1d9
2tefan <2tefan@tuta.io>
2022-04-11 12:26:14 +0200

    Rename folder from icon to header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,8 +13,8 @@
 <script setup lang="ts">
 import BackgroundMap from "./../components/BackgroundMap.vue";
 import Cards from "./../components/cards/Cards.vue";
-import CommeanLogo from "./../components/icons/CommeanLogo.vue";
-import Subtitle from "./../components/icons/Subtitle.vue";
+import CommeanLogo from "./../components/header/CommeanLogo.vue";
+import Subtitle from "./../components/header/Subtitle.vue";
 </script>
 
 <script lang="ts">
