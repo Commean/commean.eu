44d8574f5c61897f710059f2c91ad1ebde1024d6
2tefan <2tefan@tuta.io>
2022-05-05 17:36:03 +0200

    Layout on mobile

--- src/components/section/AboutSection.vue
+++ src/components/section/AboutSection.vue
@@ -6,7 +6,6 @@
       much more.
     </p>
     <div class="animation">
-      <!-- <img src="/src/assets/about/static.svg" /> -->
       <ScanCars />
     </div>
   </section>
@@ -19,17 +18,22 @@
 section {
   display: grid;
 
-  grid-template-areas: "header animation" "text animation";
-  grid-template-columns: 550px auto;
+  grid-template-areas: "header" "text" "animation";
+
+  @media (min-width: $switch-to-desktop-view) {
+    grid-template-columns: 550px auto;
+    grid-template-areas: "header animation" "text animation";
+    margin: 0 auto;
+  }
 
   max-width: 1000px;
-  margin: 75px auto;
+  padding: 50px 20px;
 
   h1 {
     grid-area: header;
     .keyword {
-        color: var(--color-text-emph);
-        font-weight: 900;
+      color: var(--color-text-emph);
+      font-weight: 900;
     }
   }
 
@@ -39,7 +43,11 @@ section {
 
   .animation {
     grid-area: animation;
-    height: 100px;
+    height: auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 }
 </style>
