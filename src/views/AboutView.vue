1b0bdfb1329fa7be90577b80900f4ac221113c9e
2tefan <2tefan@tuta.io>
2022-05-06 16:31:16 +0200

    Add contributors

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,11 @@
 <template>
   <Team />
   <FHCarinthia />
+  <Contributors />
 </template>
 
 <script setup lang="ts">
+import Contributors from "./../components/section/Contributors.vue";
 import FHCarinthia from "./../components/section/FHCarinthia.vue";
 import Team from "./../components/section/Team.vue";
 </script>
